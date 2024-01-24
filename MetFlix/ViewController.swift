d7e3e7b2198622c7a1d50c496b88943a31d3ef72
Mete <metehangurgentepe@gmail.com>
2024-01-24 13:07:29 +0300

    Initial Commit

---
+++ MetFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetFlix
+//
+//  Created by Metehan Gürgentepe on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
